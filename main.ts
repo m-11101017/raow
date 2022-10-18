2200dcc89c5ff52ee6873f8cd298c32f8d04a441
m-11101017 <115517860+m-11101017@users.noreply.github.com>
2022-10-18 10:58:16 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,36 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("C5 B A G F E D C ", 120)
+    music.playMelody("C D E F G A B C5 ", 120)
+    music.playMelody("E B C5 A B G A F ", 120)
+    music.playMelody("C5 A B G A F G E ", 120)
+    music.playMelody("G B A G C5 B A B ", 120)
+    music.playMelody("G B A G C5 B A B ", 120)
+})
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.B)) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(200)
+    }
+})
+basic.forever(function () {
+    if (input.isGesture(Gesture.Shake)) {
+        pins.servoWritePin(AnalogPin.P2, 180)
+    }
+})
+basic.forever(function () {
+    if (input.lightLevel() < 200) {
+        music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+    } else {
+        music.startMelody(music.builtInMelody(Melodies.Funeral), MelodyOptions.Once)
+    }
 })
