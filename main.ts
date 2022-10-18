5c152e2d9b299cc86a857c445be7ca9957a554b8
m-11101017 <115517860+m-11101017@users.noreply.github.com>
2022-10-18 11:18:14 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,5 @@
 input.onButtonPressed(Button.A, function () {
-    music.playMelody("C5 B A G F E D C ", 120)
-    music.playMelody("C D E F G A B C5 ", 120)
-    music.playMelody("E B C5 A B G A F ", 120)
-    music.playMelody("C5 A B G A F G E ", 120)
-    music.playMelody("G B A G C5 B A B ", 120)
-    music.playMelody("G B A G C5 B A B ", 120)
+    music.startMelody(music.builtInMelody(Melodies.Funeral), MelodyOptions.OnceInBackground)
 })
 basic.forever(function () {
     if (input.buttonIsPressed(Button.B)) {
@@ -25,6 +20,8 @@ basic.forever(function () {
 basic.forever(function () {
     if (input.isGesture(Gesture.Shake)) {
         pins.servoWritePin(AnalogPin.P2, 180)
+    } else {
+        pins.servoWritePin(AnalogPin.P2, 0)
     }
 })
 basic.forever(function () {
